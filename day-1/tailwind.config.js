88837ddb836fbad236b7b201bd1ca92f507625e0
Sostene Munezero Bagira <bagirasostene@gmail.com>
2021-10-12 00:02:43 +0200

    day-1

---
+++ day-1/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+        center: true
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
