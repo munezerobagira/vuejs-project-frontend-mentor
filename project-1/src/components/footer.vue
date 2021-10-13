ce09e2e236e796eb64d5a0b7c5ea6ee3f35ec4c7
Sostene Munezero Bagira <bagirasostene@gmail.com>
2021-10-13 23:56:17 +0200

    day 3 project 1

---
+++ project-1/src/components/footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="">
+      <div class="py-36">
+          <h1 class="text-center text-4xl font-bold">Ready To Build Your Community </h1>
+          <div class="flex justify-center items-center">
+              <button class="btn btn-primary px-10 py-4 text-white font-semibold my-8 text-lg">Get Started For Free</button>
+          </div>
+
+      </div>
+      <div id="footer">
+          <div class="container flex flex-col sm:flex-row-reverse ">
+            <div class="flex-1 my-10">
+                <h1 class="text-4xl my-2 text-white">NEWSLETTER</h1>
+                <p class="max-w-sm my-4">To receive tips on how to grow your community, signup to our weekly newsletter. We'll never send you spam or pass your email address.</p>
+                <form action="" id="subscribe-form" class="flex flex-col sm:flex-row">
+                    <input type="text" class="rounded px-10 py-2 sm:mr-3 my-2">
+                    <button class="btn btn-primary px-10 py-2 rounded sm:ml-3 my-2">subscribe</button>
+                </form>
+          </div>
+          <div class="flex-1">
+            <img src="../assets/logo.svg" class="w-36"/>
+            <p class="max-w-sm my-4">To receive tips on how to grow your community, signup to our weekly newsletter. We'll never send you spam or pass your email address.</p>
+            <div>
+                <div class="flex items-center justify-start">
+                    <img src="../assets/icon-phone.svg"  class="w-10 py-2 pr-4 x-2" alt="Phone Icon">
+                    <p>Phone: +1-543-123-4567</p>
+                </div>
+                <div class="flex items-center justify-start">
+                    <img src="../assets/icon-email.svg" class="w-10 py-2 pr-4 x-2" alt="Email Icon">
+                    <p>Email: example@huddle.com</p>
+                </div>
+                <div class="flex flex-row">
+                    <div class="w-10 py-2 pr-2"><img src="../assets/facebook-square-brands.svg" alt="Facebook icon" class="fill-current w-full text-white"></div>
+                    <div class="w-12 p-2"><img src="../assets/instagram-brands.svg" alt="Instagram icon" class="w-full"></div>
+                    <div class="w-12 p-2"><img src="../assets/twitter-square-brands.svg" alt="Twitter icon" class="w-full"></div>
+                </div>
+            </div>
+          </div>
+          </div>
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#footer{
+    @apply py-36 px-6 text-white !important;
+    background: hsl(192, 100%, 9%);
+}
+
+</style>
